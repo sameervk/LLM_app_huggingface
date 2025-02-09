e74e83e53e4d3b7b2a32ea7c0bd71b052fe6ff1e
sameervk <sameervk.iitkgp@gmail.com>
2025-02-09 16:05:46 +0000

    initial set up for containerisation

---
+++ docker_image_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Extract the latest commit hash
+LATEST_COMMIT=$(git rev-parse --short=7 HEAD)
+
+# Export the latest commit hash to the shell environment
+export LATEST_COMMIT
+
+# Print the latest commit hash
+echo "The latest commit hash is: $LATEST_COMMIT"
+
+# Build the Docker image
+docker-compose -f compose.yaml build api
+docker tag llmapp-api:latest llmapp-api:$LATEST_COMMIT
