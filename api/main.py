8beb3ea869b5054151e8a6d06240b2de8044d50a
sameervk <sameervk.iitkgp@gmail.com>
2025-04-11 09:35:12 +0100

    gh action workflow for deployment

--- api/main.py
+++ api/main.py
@@ -12,10 +12,9 @@ if env_var:
 else:
     raise FileNotFoundError("Application environment variables file not found.")
 
-app_title="Generative AI app"
+app_title = "Generative AI app"
 app_summary = "App responding to prompts. Uses GPT2 architecture"
-app = FastAPI(title=app_title,
-              summary=app_summary)
+app = FastAPI(title=app_title, summary=app_summary)
 
 origins = os.getenv("ALLOWED_ORIGINS")
 
@@ -23,8 +22,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # download model
@@ -32,20 +31,21 @@ app.add_middleware(
 # model_uri = "../mlartifacts/368316252121826279/70d9c885e3e84885b91585640a06084f/artifacts/models"
 model_uri = os.getenv("MODEL_URI")
 
-model=mlflow.pytorch.load_model(model_uri, map_location="cpu")
+model = mlflow.pytorch.load_model(model_uri, map_location="cpu")
 print(model)
+
 
 @app.get(path="/", status_code=status.HTTP_200_OK)
 async def home():
-
     return {"message": f"Hello. Welcome to {app_title}. {app_summary}"}
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app="main:app",
-                host="localhost",
-                port=5000,
-                reload=True,
-                )
+    uvicorn.run(
+        app="main:app",
+        host="localhost",
+        port=5000,
+        reload=True,
+    )
